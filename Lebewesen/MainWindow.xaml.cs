bc5ece3f0e4d2b97b9e51eab5696b4696874fdae
Burn91 <47191830+Burn91@users.noreply.github.com>
2020-10-01 20:28:35 +0200

    Initial commit

---
+++ Lebewesen/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lebewesen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ErstelleFisch(int alter, string farbe,int laenge, int maxTiefe, string name)
+        {
+            Fisch nFisch = new Fisch();
+            nFisch.Alter = alter;
+            nFisch.Farbe = farbe;
+            nFisch.Laenge = laenge;
+            nFisch.MaxTiefe = 30;
+            nFisch.Name = name;
+
+            LstBox.Items.Add(nFisch.Alter + " " +
+                             nFisch.Farbe + " " +
+                             nFisch.Laenge + " " +
+                             nFisch.MaxTiefe + " " +
+                             nFisch.Name);
+        }
+
+        public void ErstelleKatze(int alter, string farbe, int groeße, string name, string rasse)
+        {
+            Katze nKatze = new Katze();
+            {
+                nKatze.Alter = alter;
+                nKatze.Farbe = farbe;
+                nKatze.Groeße = groeße;
+                nKatze.Name = name;
+                nKatze.Rasse = rasse; 
+            }
+
+            LstBox.Items.Add(nKatze.Alter + " " +
+                             nKatze.Farbe + " " +
+                             nKatze.Groeße + " " +
+                             nKatze.Name + " " +
+                             nKatze.Rasse);
+        }
+
+        private void BtnErstelleFIsch_Click(object sender, RoutedEventArgs e)
+        {
+            ErstelleFisch(40, "grün", 30, 40, "Fischibert");
+        }
+
+        private void btnErstelleKatze_Click(object sender, RoutedEventArgs e)
+        {
+            ErstelleKatze(13, "braun", 20, "Schnurli", "BKH");
+        }
+    }
+}
---
+++ Lebewesen/cLebewesen.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lebewesen
+{
+   abstract class CLebewesen
+    {
+
+        string name;
+        string farbe;
+        int alter;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return this.name; }
+           
+        }
+
+        public string Farbe
+        {
+            set { farbe = value; }
+            get { return this.farbe; }
+        
+        }
+
+        public int Alter
+        {
+            set { alter = value; }
+            get { return this.alter; }
+            
+        }
+    }
+
+
+    class Katze : CLebewesen
+    {
+
+        string rasse;
+        int groeße;
+
+        public string Rasse
+        {
+            set { rasse = value; }
+            get { return this.rasse; }
+           
+        }
+
+        public int Groeße
+        {
+            set { groeße = value; }
+            get { return this.groeße; }
+         
+        }
+        
+    }
+
+    class Fisch : CLebewesen
+    {
+        int maxTiefe;
+        int laenge;
+      
+        public int MaxTiefe
+        {
+            set { maxTiefe = value; }
+            get { return this.maxTiefe; }
+            
+        }
+
+        public int Laenge
+        {
+            set { laenge = value; }
+            get { return this.laenge; }
+
+        }
+
+
+    }
+}
